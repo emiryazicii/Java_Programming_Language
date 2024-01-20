b95e2fe4081bb47348be0418176f0dc9f3a7687f
European01 <emiryaziciofficial@gmail.com>
2024-01-20 11:37:53 -0500

    Added an example to practice.

--- src/lesson6_ifStatements/NumberOfDays.java
+++ src/lesson6_ifStatements/NumberOfDays.java
@@ -1,4 +1,24 @@
 package lesson6_ifStatements;
 
 public class NumberOfDays {
+
+    public static void main(String[] args) {
+
+        int month = 5;
+
+        boolean has28Days = month ==2;
+        boolean has30Days = month ==4 || month ==6 || month ==9 || month ==11;
+        boolean has31Days = !has28Days && !has30Days;
+
+        String temp = "";
+
+        if (has28Days){ temp = "28 Days";}
+
+        if (has30Days){ temp = "30 Days";}
+
+        if (has31Days){ temp = "31 Days";}
+
+        System.out.println(temp);
+
+    }
 }
