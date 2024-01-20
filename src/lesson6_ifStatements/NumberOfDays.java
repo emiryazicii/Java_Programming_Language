e79d70aa1401e3c029aa38596aa1737fa65bf79d
European01 <emiryaziciofficial@gmail.com>
2024-01-20 11:38:50 -0500

    Added requirements for the example task.

--- src/lesson6_ifStatements/NumberOfDays.java
+++ src/lesson6_ifStatements/NumberOfDays.java
@@ -22,3 +22,20 @@ public class NumberOfDays {
 
     }
 }
+/*
+1. Create a class named NumberOfDays.java
+2. An integer variable named month is declared and given, Write a
+program that can print the number of days in the given month
+
+Ex:
+Given:
+number = 1
+
+output:
+31 days
+
+Hints:
+Months that has 31 days are: 1, 3, 5, 7, 8, 10, 12
+Months that has 30 days are: 4, 6, 9, 11
+Month that has 28 days: 2
+ */
