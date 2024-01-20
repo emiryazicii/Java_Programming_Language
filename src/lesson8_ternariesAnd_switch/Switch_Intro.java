bef00c8c89dcc82780db4f193634ba470b935122
European01 <emiryaziciofficial@gmail.com>
2024-01-20 12:26:52 -0500

    Added an example to practice.

--- src/lesson8_ternariesAnd_switch/Switch_Intro.java
+++ src/lesson8_ternariesAnd_switch/Switch_Intro.java
@@ -1,4 +1,34 @@
 package lesson8_ternariesAnd_switch;
 
 public class Switch_Intro {
+
+    public static void main(String[] args) {
+
+        char grade = 'B';
+
+        switch (grade) {
+            case 'A':
+                System.out.println("Excellent");
+                break;
+
+            case 'B':
+                System.out.println("Good Job");
+                break;
+
+            case 'C':
+                System.out.println("Good");
+                break;
+
+            case 'D':
+                System.out.println("Passed");
+                break;
+
+            case 'F':
+                System.out.println("Failed");
+                break;
+
+            default :
+                System.out.println("Invalid Entry");
+        }
+    }
 }
