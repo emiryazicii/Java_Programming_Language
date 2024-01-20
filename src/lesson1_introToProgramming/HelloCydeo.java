dc184b74926da5f39a058b3318d3e0bc8db52506
European01 <emiryaziciofficial@gmail.com>
2024-01-20 00:07:18 -0500

    Added some examples to explain how to print statement works.

--- src/lesson1_introToProgramming/HelloCydeo.java
+++ src/lesson1_introToProgramming/HelloCydeo.java
@@ -1,4 +1,15 @@
 package lesson1_introToProgramming;
 
 public class HelloCydeo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Cydeo");
+        System.out.println();
+        System.out.println("How are you ?");
+        System.out.println();
+        System.out.println("I love Java");
+        System.out.println();
+        System.out.println("Hello Cydeo\n\nHow are you?\n\nI love java");
+    }
 }
