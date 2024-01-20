2bad6d76e723a6327acc92d054bb4b200e97a86e
European01 <emiryaziciofficial@gmail.com>
2024-01-20 12:45:24 -0500

    Added an example to practice.

--- src/lesson9_scanner/NextLineVsNext.java
+++ src/lesson9_scanner/NextLineVsNext.java
@@ -1,4 +1,23 @@
 package lesson9_scanner;
 
+import java.util.Scanner;
+
 public class NextLineVsNext {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your age: ");
+
+        int age = input.nextInt();
+
+        input.nextLine();
+
+        System.out.println("Enter your full name: ");
+        String fullName = input.nextLine();
+
+        System.out.println("Your age is: " + age);
+        System.out.println("Your name is: " + fullName);
+    }
 }
