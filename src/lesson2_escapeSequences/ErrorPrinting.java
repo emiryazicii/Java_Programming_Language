3937e714b065418c8a411fcb613bdfc14f3ab52a
European01 <emiryaziciofficial@gmail.com>
2024-01-20 00:20:14 -0500

    Added an example to show how to print an error message.

--- src/lesson2_escapeSequences/ErrorPrinting.java
+++ src/lesson2_escapeSequences/ErrorPrinting.java
@@ -1,4 +1,9 @@
 package lesson2_escapeSequences;
 
 public class ErrorPrinting {
+
+    public static void main(String[] args) {
+
+        System.err.println("Hello World , How are you today?"); // short-cut ==> serr + enter
+    }
 }
