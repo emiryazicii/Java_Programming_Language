925729c7f7e28861515790d8424423bd0eb9a1b8
European01 <emiryaziciofficial@gmail.com>
2024-01-20 11:52:33 -0500

    Added and example practice.

--- src/lesson7_ifStatements/GradeLevel.java
+++ src/lesson7_ifStatements/GradeLevel.java
@@ -1,4 +1,28 @@
 package lesson7_ifStatements;
 
 public class GradeLevel {
+
+    public static void main(String[] args) {
+
+        int gradeLevel = 11;
+
+        String temp = "";
+
+        if (gradeLevel <= 5) {
+            temp = "Elementary School";
+        } else if (gradeLevel <= 8) {
+            temp = "Middle School";
+
+        } else if (gradeLevel <= 12) {
+            temp = "High School";
+
+        } else if (gradeLevel <= 16) {
+            temp = "College";
+
+        } else {
+            temp = "Grad School";
+        }
+
+        System.out.println(temp);
+    }
 }
