c71c2be9ec2fed06edc0801ce4debb1e82bf1e45
European01 <emiryaziciofficial@gmail.com>
2024-01-20 12:49:14 -0500

    Added an example to practice.

--- src/lesson9_scanner/NumberOfDays.java
+++ src/lesson9_scanner/NumberOfDays.java
@@ -1,4 +1,27 @@
 package lesson9_scanner;
 
 public class NumberOfDays {
+
+    public static void main(String[] args) {
+
+        int month = 7;
+
+        String temp = "";
+
+        switch (month) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                temp = "31 Days";
+                break;
+            case 4: case 6: case 9: case 11:
+                temp = "30 Days";
+                break;
+            case 2:
+                temp = "28 Days";
+                break;
+            default:
+                temp = "Invalid Entry";
+
+        }
+        System.out.println(temp);
+    }
 }
