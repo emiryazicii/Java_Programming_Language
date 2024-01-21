164fb2eb6217abf8c8fc1a41a0b95f1aa52f0c21
European01 <emiryaziciofficial@gmail.com>
2024-01-21 18:53:05 -0500

    Add some examples to practice.

--- src/lesson11_forLoop/ForLoopPractice.java
+++ src/lesson11_forLoop/ForLoopPractice.java
@@ -1,4 +1,42 @@
 package lesson11_forLoop;
 
 public class ForLoopPractice {
+
+    public static void main(String[] args) {
+
+        for (int i = 10; i >= 5; i--) {
+
+            System.out.println("Cydeo");
+
+        }
+        System.out.println("------------------------------------");
+
+        int sum = 0;
+
+
+        for (int a = 1; a <= 100; a++) {
+
+            sum += a;
+
+        }
+
+        System.out.println(sum);
+
+        System.out.println("------------------------------------------------------------");
+
+        for(char b = 'A'; b <= 'Z'; b++){
+
+            System.out.print(b+" ");
+
+        }
+        System.out.println();
+
+        System.out.println("Hello");
+
+        System.out.println("---------------------------------------------------------------");
+
+        for (char c = 'Z'; c >= 'A'; c--){
+            System.out.print(c+" ");
+        }
+    }
 }
