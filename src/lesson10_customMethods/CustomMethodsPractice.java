2c3ff10500132dacb62b2605519647956e7849a5
European01 <emiryaziciofficial@gmail.com>
2024-01-20 21:09:10 -0500

    Add some methods.

--- src/lesson10_customMethods/CustomMethodsPractice.java
+++ src/lesson10_customMethods/CustomMethodsPractice.java
@@ -1,4 +1,37 @@
 package lesson10_customMethods;
 
 public class CustomMethodsPractice {
+
+    public static void main(String[] args) {
+
+        maxNum(10.5, 52.856);
+        System.out.println("-----------------------------------------");
+
+        maxNum(50_000, 61_000);
+        System.out.println("-------------------------------------------");
+
+        maxNum(-200, -78);
+        System.out.println("-----------------------------------");
+
+        initial("emir","yazici");
+
+    }
+
+    public static void maxNum(double num1, double num2) {
+
+        if (num1 > num2) {
+            System.out.println(num1 + " is the maximum number");
+        } else if (num2 > num1) {
+            System.out.println(num2 + " is the maximum number");
+        } else {
+            System.out.println("Numbers are equal");
+        }
+    }
+
+    public static void initial(String firstname, String lastname) {
+
+        String initials = firstname.substring(0,1).toUpperCase() + "." + lastname.substring(0,1).toUpperCase();
+
+        System.out.println(initials);
+    }
 }
