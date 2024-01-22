1c9b7daadf427a47500b5cd46b1f7c60f896244f
European01 <emiryaziciofficial@gmail.com>
2024-01-21 19:21:33 -0500

    Add an example to practice.

--- src/lesson12_whileLoop/RemoveDuplicates.java
+++ src/lesson12_whileLoop/RemoveDuplicates.java
@@ -1,4 +1,32 @@
 package lesson12_whileLoop;
 
 public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+
+        String str = "xxzzzvvtttyyyyy";
+
+        String temp = ""; // "abc"
+
+        for (int i = 0; i < str.length() ; i++) { // i : index number of str
+
+            char each = str.charAt(i); // each character of the str
+
+            if (!temp.contains(""+each)){// if the string temp doesn't contain the character of string str yet
+                temp += each; // then we will add the character to string result
+
+            }
+        }
+
+        System.out.println(temp);
+    }
 }
+/*
+Write a program that can remove the duplicated characters from the a string
+
+				Ex:
+					str = "aabbbcccc"
+
+					output:
+						abc
+ */
