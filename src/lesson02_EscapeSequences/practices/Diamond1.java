b391f1561e6bff5e054ac4547794dcccac695db1
European01 <emiryaziciofficial@gmail.com>
2024-01-30 15:52:41 -0500

    Add an example to practice.

--- src/lesson02_EscapeSequences/practices/Diamond1.java
+++ src/lesson02_EscapeSequences/practices/Diamond1.java
@@ -1,4 +1,38 @@
 package lesson02_EscapeSequences.practices;
 
 public class Diamond1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("\t\t\t    ^");
+        System.out.println("\t\t\t   / \\");
+        System.out.println("\t\t\t  /   \\");
+        System.out.println("\t\t\t /     \\");
+        System.out.println("\t\t\t/       \\");
+        System.out.println("\t\t\t---------");
+        System.out.println("\t\t\t\\       /");
+        System.out.println("\t\t\t \\     /");
+        System.out.println("\t\t\t  \\   /");
+        System.out.println("\t\t\t   \\ /");
+        System.out.println("\t\t\t    v");
+
+        System.out.println("\t\t\t    ^\n\t\t\t   / \\\n\t\t\t  /   \\\n\t\t\t /     \\\n\t\t\t/       \\\n\t\t\t---------"+
+                "\n\t\t\t\\       /\n\t\t\t \\     /\n\t\t\t  \\   /\n\t\t\t   \\ /\n\t\t\t    v");
+    }
 }
+/*
+Create a class named Diamond1.java
+   Write a code to print the following output in the console:
+   		        ^
+			   / \
+			  /   \
+			 /     \
+			/       \
+			---------
+			\       /
+			 \     /
+			  \   /
+			   \ /
+			    v
+ */
+
