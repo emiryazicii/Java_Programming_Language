aa4a86db39bd2c6ac256de92a2965f6405095caa
European01 <emiryaziciofficial@gmail.com>
2024-01-30 17:34:44 -0500

    Add an example to practice.

--- src/lesson08_Scanner/practices/Circle.java
+++ src/lesson08_Scanner/practices/Circle.java
@@ -1,4 +1,28 @@
 package lesson08_Scanner.practices;
 
+import java.util.Scanner;
+
 public class Circle {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the radius of the circle: ");
+
+        double radius = input.nextDouble(),
+                area = 3.14 * (radius * radius),
+                perimeter = 2 * 3.14 * radius ;
+
+        System.out.println("Area of the circle is: " + area + "\nPerimeter of the circle is: " + perimeter);
+
+        input.close();
+    }
 }
+/*
+ Create a class named Circle:
+            1.1 Ask the user to enter the radius of the circle
+            1.1 Calculate the area and perimeter of the circle by using the radius
+
+ */
+
