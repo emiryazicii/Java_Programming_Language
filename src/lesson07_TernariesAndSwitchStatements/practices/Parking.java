2ae59a703a2251818b4010680786cb4b7a1d9a49
European01 <emiryaziciofficial@gmail.com>
2024-01-30 17:56:56 -0500

    Add an example to practice.

--- src/lesson07_TernariesAndSwitchStatements/practices/Parking.java
+++ src/lesson07_TernariesAndSwitchStatements/practices/Parking.java
@@ -1,4 +1,36 @@
 package lesson07_TernariesAndSwitchStatements.practices;
 
 public class Parking {
+
+    public static void main(String[] args) {
+
+        double time = 23.59,
+                normalFee = 7.5,
+                doubleFee = normalFee * 2;
+
+        boolean normalRateActive = time >= 6.00 && time < 13.00,
+                doubleRateActive= time >= 13.00 && time < 24.00 || time >= 0.00 && time <= 5.00,
+                outOfRange = time >=24.00 || time <0.00;
+
+        String temp =(normalRateActive )? "Fee is: " + normalFee
+                     :(doubleRateActive)? "Fee is: " + doubleFee : "";
+
+        if(outOfRange){
+            System.err.println("Error Occurred");
+        }
+        System.out.println(temp);
+    }
 }
+
+/*
+create a program that will define a time variable. Use a 24 hour format. Based on the given time determine the cost of parking in the city
+
+		If the time is between 6-12:
+			fee: 7.50
+
+		if the time is between 13 - 23 or 0 - 5
+			fee is 15
+
+		extra: if the time is outside of the valid range (0-24) then output an error message
+ */
+
