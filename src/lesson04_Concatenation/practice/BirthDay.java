123550b31e0ca7577bb7f16a863da4c290cef6d8
European01 <emiryaziciofficial@gmail.com>
2024-01-30 16:01:11 -0500

    Add an example to practice.

--- src/lesson04_Concatenation/practice/BirthDay.java
+++ src/lesson04_Concatenation/practice/BirthDay.java
@@ -1,4 +1,30 @@
 package lesson04_Concatenation.practice;
 
 public class BirthDay {
+
+    public static void main(String[] args) {
+
+        String name = "John",
+                birthMonth = "April";
+
+        int birthDay = 25,
+                birthYear = 1995;
+
+        System.out.println(name + " was born on " + birthMonth + "/" + birthDay + "/" + birthYear + ".");
+    }
 }
+/*
+Create a class called BirthDay and create the following variable
+             name, birthDay(int), birthMonth(String), birthYear(int)
+
+             use concatenation to display the birthday of the person
+
+                 if  name = "John"
+                     birthDay = 25
+                     birthMonth = "April"
+                     birthYear = 1995;
+
+                 output:
+                     John was born on April/25/1995.
+ */
+
