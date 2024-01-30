cd771a3759182bf4dda8d7e94b2a04cdd8c3839e
European01 <emiryaziciofficial@gmail.com>
2024-01-30 15:47:01 -0500

    Add an example to practice.

--- src/lesson01_IntroToProgramming/practices/JavaComponents.java
+++ src/lesson01_IntroToProgramming/practices/JavaComponents.java
@@ -1,4 +1,21 @@
 package lesson01_IntroToProgramming.practices;
 
 public class JavaComponents {
+
+    public static void main(String [] args ) {
+        System.out.println("JDK:Java Development Kit");
+        System.out.println("JRE:Java Runtime Environment");
+        System.out.println("JVM:Java Virtual Machine");
+
+        System.out.println("JDK:Java Development Kit\nJRE:Java Runtime Environment\nJVM:Java Virtual Machine");
+    }
 }
+/*
+Create a class named JavaComponents.java
+Print the full form of each of the followings:
+
+1. JDK
+2. JRE
+3. JVM
+*/
+
