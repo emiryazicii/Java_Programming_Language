6b817003c769abd1436da946beb9c4ceab2fde11
European01 <emiryaziciofficial@gmail.com>
2024-01-30 16:19:01 -0500

    Add an example to practice.

--- src/lesson06_IfStatements/practices/CharacterIdentity.java
+++ src/lesson06_IfStatements/practices/CharacterIdentity.java
@@ -1,4 +1,30 @@
 package lesson06_IfStatements.practices;
 
 public class CharacterIdentity {
+
+    public static void main(String[] args) {
+
+        char ch = '!';
+
+        boolean digit = ch >= '0' && ch <= '9',
+                alphabetic = (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'),
+                special = !digit && !alphabetic;
+
+        String temp = "";
+
+
+        if (digit) {
+            temp = "Digit";
+        }
+
+        if (alphabetic) {
+            temp = "Alphabetic Character";
+        }
+
+        if (special) {
+            temp = "Special Character";
+        }
+
+        System.out.println(temp);
+    }
 }
