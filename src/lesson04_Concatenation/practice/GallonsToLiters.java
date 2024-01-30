d6a6bb8ab90521d68fd50111b76bbbc4bc0ef072
European01 <emiryaziciofficial@gmail.com>
2024-01-30 16:04:23 -0500

    Add an example to practice.

--- src/lesson04_Concatenation/practice/GallonsToLiters.java
+++ src/lesson04_Concatenation/practice/GallonsToLiters.java
@@ -1,4 +1,28 @@
 package lesson04_Concatenation.practice;
 
 public class GallonsToLiters {
+
+    public static void main(String[] args) {
+
+        double gallon = 10,
+                litters = gallon * 3.79;
+
+        System.out.println(gallon + " gallons is equal to " + litters + " litters");
+    }
 }
+/*
+Create a class named GallonsToLiters and declare the following variables:
+                gallon
+                liters
+
+    4.1 Write a program that can convert the any given number of gallons to liters
+        Ex:
+              gallon = 10
+
+        output:
+              10 gallons is equal to 37.9 litters
+
+           Hint:  1 gal = 3.79 L
+
+ */
+
