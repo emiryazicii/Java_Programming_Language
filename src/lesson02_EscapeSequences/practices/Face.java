28e5040f6a2005909da776ef599b42ee6e12d1db
European01 <emiryaziciofficial@gmail.com>
2024-01-30 15:54:10 -0500

    Add an example to practice.

--- src/lesson02_EscapeSequences/practices/Face.java
+++ src/lesson02_EscapeSequences/practices/Face.java
@@ -1,4 +1,28 @@
 package lesson02_EscapeSequences.practices;
 
 public class Face {
+
+    public static void main(String[] args) {
+
+        System.out.println("\t +\"\"\"\"\"+");
+        System.out.println("\t[| o o |]");
+        System.out.println("\t |  ^  |");
+        System.out.println("\t | '-' |");
+        System.out.println("\t +-----+");
+
+        System.out.println("\t +\"\"\"\"\"+\n\t[| o o |]\n\t |  ^  |\n\t | '-' |\n\t +-----+");
+    }
 }
+
+/*
+Create a class named Face.java
+   Write a code to print the following output in the console:
+
+     +"""""+
+	[| o o |]
+	 |  ^  |
+	 | '-' |
+	 +-----+
+
+ */
+
