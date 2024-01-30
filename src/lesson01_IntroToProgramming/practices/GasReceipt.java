9910e2cb8ad17d8cb501ed94753dc22f5b57438b
European01 <emiryaziciofficial@gmail.com>
2024-01-30 15:46:01 -0500

    Add an example to practice.

--- src/lesson01_IntroToProgramming/practices/GasReceipt.java
+++ src/lesson01_IntroToProgramming/practices/GasReceipt.java
@@ -1,4 +1,32 @@
 package lesson01_IntroToProgramming.practices;
 
 public class GasReceipt {
+
+    public static void main(String [] args ) {
+
+        System.out.println("    MCLEAN STORE    ");
+        System.out.println();
+        System.out.println("04-29-2022   02:45 pm");
+        System.out.println();
+        System.out.println("Gallons:        11.48");
+        System.out.println("Price/gallon:   $4.15");
+        System.out.println();
+        System.out.println("Fuel total:     $47.642");
+
+        System.out.println("    MCLEAN STORE    \n\n04-29-2022  02:45 pm\n\nGallons:        "+
+                "11.48\nPrice/Gallon:   $4.15\n\nFuel Total:     $47.642");
+    }
 }
+/*
+Create a class named GasReceipt.java
+   Write a code to print the following output in the console:
+    MCLEAN STORE
+
+04-29-2022   02:45 pm
+
+Gallons:        11.48
+Price/gallon:   $4.15
+
+Fuel total:     $47.642
+*/
+
