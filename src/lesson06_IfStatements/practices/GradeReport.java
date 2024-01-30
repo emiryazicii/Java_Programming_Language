2ef5cbe79cce916ea07c76cc2d301636d4f1a913
European01 <emiryaziciofficial@gmail.com>
2024-01-30 16:32:50 -0500

    Add an example to practice.

--- src/lesson06_IfStatements/practices/GradeReport.java
+++ src/lesson06_IfStatements/practices/GradeReport.java
@@ -1,4 +1,31 @@
 package lesson06_IfStatements.practices;
 
 public class GradeReport {
+
+    public static void main(String[] args) {
+
+        int score = 1;
+
+        boolean valid = score >= 0 && score <= 100,
+                scoreTo100 = score >= 90, scoreTo90 = score >= 80,
+                scoreTo80 = score >= 70,scoreTo70 =  score >= 60 ;
+
+        String temp = "";
+
+        if (!valid) {
+            temp = "Invalid Entry";
+        } else if (scoreTo100) {
+            temp = "Your grade is A";
+        } else if (scoreTo90) {
+            temp = "Your grade is B";
+        } else if (scoreTo80) {
+            temp = "Your grade is C";
+        } else if (scoreTo70) {
+            temp = "Your grade is D";
+        } else {
+            temp += "Your grade is F";
+        }
+
+        System.out.println(temp);
+    }
 }
