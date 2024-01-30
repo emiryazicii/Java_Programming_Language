44d774cab07585a9c42cbd506d5b31bedf0e14d7
European01 <emiryaziciofficial@gmail.com>
2024-01-30 17:22:23 -0500

    Add an example to practice.

--- src/lesson07_TernariesAndSwitchStatements/practices/SalesBonus.java
+++ src/lesson07_TernariesAndSwitchStatements/practices/SalesBonus.java
@@ -1,4 +1,35 @@
 package lesson07_TernariesAndSwitchStatements.practices;
 
 public class SalesBonus {
+
+    public static void main(String[] args) {
+
+        int salesAmount = 46_000,
+                bonus = 0;
+
+        boolean bonus5K = (salesAmount >= 10_000 && salesAmount < 15_000),
+                noBonus = (salesAmount < 10_000);
+
+        String result = (noBonus) ? ("You have " + bonus + " bonus.")
+                : (bonus5K) ? ("You have " + 5000 + " bonus.")
+                : ("You have " + 7000 + " bonus.");
+
+        System.out.println(result);
+
+    }
 }
+/*
+Sales Bonus [ternary]
+
+	create a sales amount variable
+	use the sales amount to determine the bonus you get at end of the month
+
+		if your sales amount is less than 10000 you don't get any bonus
+
+		if your sales amount is more than or equal to 10000 and less than 15000 you get a bonus of 5000
+
+		if your sales amount is more than or equal to 15000 you get a bonus of 7000
+
+		print your bonus number
+ */
+
