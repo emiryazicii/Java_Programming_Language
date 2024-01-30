71015c05fe5f554de8f26780f25b04e3cec34610
European01 <emiryaziciofficial@gmail.com>
2024-01-30 16:07:06 -0500

    Add an example to practice.

--- src/lesson04_Concatenation/practice/SalaryCalculator.java
+++ src/lesson04_Concatenation/practice/SalaryCalculator.java
@@ -1,4 +1,41 @@
 package lesson04_Concatenation.practice;
 
 public class SalaryCalculator {
+
+    public static void main(String[] args) {
+
+        String name = "Joshua";
+
+        double hourlyRate = 40,
+                weeklyHours = 45,
+                salary = hourlyRate * weeklyHours * 52 ;
+
+        System.out.println("\tHello " + name + ", your salary is $ " + salary);
+
+    }
 }
+/*
+Create a class named SalaryCalculator.java
+   Declare the following Variables:
+
+1. name
+2. hourlyRate
+3. weeklyHours
+
+   Write a program that can calculate the salary, and display the following info:
+
+   Hint: there are 52 weeks in a year
+
+   salary = hourlyRate * weeklyHours * 52
+
+Ex:
+given data:
+
+name = “Joshua”
+hourlyRate = 40
+weeklyHours = 45
+
+output:
+Hello Joshua, your salary is $ 93600
+ */
+
