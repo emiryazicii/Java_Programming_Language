8ad2417d76ac61d4c4d7ba78c4db16506fcdd7b2
European01 <emiryaziciofficial@gmail.com>
2024-02-01 00:47:47 -0500

    Add an example to practice.

--- src/lesson11_ForLoop/practices/Sum.java
+++ src/lesson11_ForLoop/practices/Sum.java
@@ -1,4 +1,30 @@
 package lesson11_ForLoop.practices;
 
 public class Sum {
+
+    public static void main(String[] args) {
+
+        System.out.println(sum(100));
+        System.out.println(sum(50));
+
+    }
+    public static int sum(int num) {
+
+        int sum = 0;
+
+        for (int i = 1; i <= num; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 }
+/*
+3. write a program that can calculate the sum of all numbers between 1 to any given number
+            ex:
+                input: 100
+                output: 5050
+
+                input: 50
+                output: 1275
+ */
+
