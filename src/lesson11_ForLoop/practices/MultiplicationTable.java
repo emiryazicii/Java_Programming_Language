699876553c388b45efb752c0cc60a2bf383828c9
European01 <emiryaziciofficial@gmail.com>
2024-02-01 00:31:38 -0500

    Add an example to practice.

--- src/lesson11_ForLoop/practices/MultiplicationTable.java
+++ src/lesson11_ForLoop/practices/MultiplicationTable.java
@@ -1,4 +1,49 @@
 package lesson11_ForLoop.practices;
 
 public class MultiplicationTable {
+
+    public static void main(String[] args) {
+
+        System.out.println(displayMultiplicationTable(5));
+        System.out.println(displayMultiplicationTable(5,20));
+    }
+    public static String displayMultiplicationTable (int num){
+
+        String temp = "";
+
+        for (int i = 1; i <= 10  ; i++) {
+
+            temp += num +" x "+ i +" = "+ (num*i) +"\n" ;
+        }
+        return temp;
+    }
+    public static String displayMultiplicationTable (int num, int multiplier){
+
+        String temp = "";
+
+        for (int i = 1; i <= multiplier ; i++) {
+
+            temp += num +" x "+ i +" = "+ (num*i) +"\n" ;
+        }
+        return temp;
+    }
 }
+/*
+Multiplication Table [methods, loops, operators]
+
+	create a method that will accept a number. Print the multiplication table for the give number up until 10
+
+	print the results in this format:
+		$number x 1 = result
+		$number x 2 = result
+		...
+		$number x 10 = result
+
+		ex:
+			number = 4
+			4 x 1 = 4
+			4 x 2 = 8
+			...
+
+ */
+
