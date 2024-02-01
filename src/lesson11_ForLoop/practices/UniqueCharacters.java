57bf9f59ef45fd7df3a0871aafb74bad5a1a9112
European01 <emiryaziciofficial@gmail.com>
2024-02-01 01:00:05 -0500

    Add an example to practice.

--- src/lesson11_ForLoop/practices/UniqueCharacters.java
+++ src/lesson11_ForLoop/practices/UniqueCharacters.java
@@ -1,4 +1,36 @@
 package lesson11_ForLoop.practices;
 
 public class UniqueCharacters {
+
+    public static void main(String[] args) {
+
+        System.out.println(displayUniqueCharacters("AABCCDEEEFGGHTTTUVVYZZZ"));
+
+    }
+    public static String displayUniqueCharacters(String str){
+
+        String unique = "";
+
+        for (int i = 0; i <= str.length()-1; i++) {
+
+            char c = str.charAt(i);
+
+            if (str.indexOf(c) == str.lastIndexOf(c)){
+
+                unique += c;
+            }
+        }
+        return unique;
+    }
 }
+/*
+9. Create a class called UniqueCharacters, Write a program that can return the unique characters from a String
+
+	            Ex:
+	                input:
+	                    AABCCD
+
+	                output:
+	                    BD
+ */
+
