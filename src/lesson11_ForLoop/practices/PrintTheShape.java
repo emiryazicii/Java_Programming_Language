a52df14b1864c4b37b41d06f7b2d896c38408460
European01 <emiryaziciofficial@gmail.com>
2024-02-01 00:43:50 -0500

    Add an example to practice.

--- src/lesson11_ForLoop/practices/PrintTheShape.java
+++ src/lesson11_ForLoop/practices/PrintTheShape.java
@@ -1,4 +1,30 @@
 package lesson11_ForLoop.practices;
 
 public class PrintTheShape {
+
+    public static void main(String[] args) {
+
+        String star = "* ",
+                shape ="";
+
+        for (int i = 0; i <= 5; i++) {
+            shape += star ;
+        }
+        for (int i = 0; i <8 ; i++) {
+            System.out.println("\t\t\t"+shape);
+        }
+    }
 }
+/*
+1. Use a loop to print the following shape:
+
+                * * * * * *
+                * * * * * *
+                * * * * * *
+                * * * * * *
+                * * * * * *
+                * * * * * *
+                * * * * * *
+                * * * * * *
+ */
+
