3451289c4b441a8bd19b3976f4c7df020dd1ce45
European01 <emiryaziciofficial@gmail.com>
2024-01-31 23:20:42 -0500

    Add an example to practice.

--- src/lesson09_String/practices/MoveFirstWord.java
+++ src/lesson09_String/practices/MoveFirstWord.java
@@ -1,4 +1,32 @@
 package lesson09_String.practices;
 
+import java.util.Scanner;
+
 public class MoveFirstWord {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a sentence: ");
+        String sentence = input.nextLine().trim();
+        input.close();
+
+        String firstWord = sentence.substring(0,sentence.indexOf(" ")),
+                newSentence = sentence.substring(sentence.indexOf(" ")+1)+" "+firstWord;
+
+        System.out.println(newSentence);
+    }
 }
+
+/*
+T4MoveFirstWord [String, Scanner]
+
+    create a program that will ask the user to enter a sentence. Print the sentence with the first word moved to the end
+
+    Input:
+        Java is a fun language
+    Output:
+        is a fun language Java
+ */
+
