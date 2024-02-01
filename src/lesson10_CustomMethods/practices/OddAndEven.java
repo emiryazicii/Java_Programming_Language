f357e0a55a81919aa79a2c45e1be3803ca7131da
European01 <emiryaziciofficial@gmail.com>
2024-02-01 00:06:06 -0500

    Add an example to practice.

--- src/lesson10_CustomMethods/practices/OddAndEven.java
+++ src/lesson10_CustomMethods/practices/OddAndEven.java
@@ -1,4 +1,26 @@
 package lesson10_CustomMethods.practices;
 
 public class OddAndEven {
+
+    public static void main(String[] args) {
+
+        System.out.println(isOdd(23));
+        System.out.println(isEven(22));
+    }
+
+    public static boolean isOdd(int number) {
+
+        return (number % 2 != 0) ;
+    }
+
+    public static boolean isEven(int number) {
+
+        return !isOdd(number);
+    }
 }
+/*
+1. Create a method named isOdd, that can return true if a number is an odd number, otherwise returns false
+
+2. Create a method named isEven, that can return true if a number is an even number, otherwise returns false
+ */
+
