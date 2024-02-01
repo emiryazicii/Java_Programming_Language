97d9ed329ef36b6d70dc5609a8befb4563ad2254
European01 <emiryaziciofficial@gmail.com>
2024-02-01 00:51:13 -0500

    Add an example to practice.

--- src/lesson11_ForLoop/practices/SumOfDigits.java
+++ src/lesson11_ForLoop/practices/SumOfDigits.java
@@ -1,4 +1,41 @@
 package lesson11_ForLoop.practices;
 
 public class SumOfDigits {
+
+    public static void main(String[] args) {
+
+        System.out.println(sumOfDigits("A1B2C3"));
+        System.out.println(sumOfDigits("007JamesBond0123"));
+        System.out.println(sumOfDigits("B2C3D4F5H11J"));
+        System.out.println(sumOfDigits("111111111"));
+        System.out.println(sumOfDigits("123456789123456789"));
+
+    }
+    public static int sumOfDigits(String str) {
+        int sum = 0;
+
+        for (int i = 0; i <= 9; i++) {
+            char c = (char) (i + '0');
+
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == c) {
+                    sum += i;
+                }
+            }
+        }
+        return sum;
+    }
 }
+/*
+6. Write a program that can return the sum of digits from a  string
+             Ex:
+                 input: A1B2C3
+
+                 output:    6
+
+             Hint: You need to get each of the character by using a loop
+	             	To convert char to number:
+						             	   '0' - 48   ==> 0
+						             	   '1' - 48   ==> 1
+ */
+
