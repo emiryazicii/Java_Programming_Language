749551823db1251fa8f93c79268486fcdce7a365
European01 <emiryaziciofficial@gmail.com>
2024-02-03 20:44:41 -0500

    Update line 37.

--- src/lesson11_ForLoop/practices/CountHi.java
+++ src/lesson11_ForLoop/practices/CountHi.java
@@ -34,7 +34,7 @@ public class CountHi {
 
         for (int i = 0; i <= str1.length() - str2.length(); i++) {
 
-            if(str1.substring(i,i+3).equalsIgnoreCase(str2)){
+            if(str1.substring(i,i+str2.length()).equalsIgnoreCase(str2)){
                 count++;
             }
         }
