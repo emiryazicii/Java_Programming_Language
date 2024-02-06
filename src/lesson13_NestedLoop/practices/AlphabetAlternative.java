4041f042449dfbf77a57467cde6f6f187032e82c
European01 <emiryaziciofficial@gmail.com>
2024-02-05 20:14:53 -0500

    Add an example to practice.

--- src/lesson13_NestedLoop/practices/AlphabetAlternative.java
+++ src/lesson13_NestedLoop/practices/AlphabetAlternative.java
@@ -1,4 +1,33 @@
 package lesson13_NestedLoop.practices;
 
 public class AlphabetAlternative {
+
+    public static void main(String[] args) {
+
+        char firstLetter = 'A';
+
+        while (firstLetter <= 'E') {
+
+            char secondLetter = 'a';
+
+            while (secondLetter <= 'z') {
+
+                System.out.print(""+firstLetter+secondLetter+" ");
+
+                secondLetter++;
+            }
+            firstLetter++;
+
+            System.out.println();
+        }
+    }
 }
+/*
+ Use a nested loop to print the following output:
+        Aa Ab Ac Ad Ae Af Ag Ah Ai Aj Ak Al Am An Ao Ap Aq Ar As At Au Av Aw Ax Ay Az
+        Ba Bb Bc Bd Be Bf Bg Bh Bi Bj Bk Bl Bm Bn Bo Bp Bq Br Bs Bt Bu Bv Bw Bx By Bz
+        Ca Cb Cc Cd Ce Cf Cg Ch Ci Cj Ck Cl Cm Cn Co Cp Cq Cr Cs Ct Cu Cv Cw Cx Cy Cz
+        Da Db Dc Dd De Df Dg Dh Di Dj Dk Dl Dm Dn Do Dp Dq Dr Ds Dt Du Dv Dw Dx Dy Dz
+        Ea Eb Ec Ed Ee Ef Eg Eh Ei Ej Ek El Em En Eo Ep Eq Er Es Et Eu Ev Ew Ex Ey Ez
+ */
+
