4d3d15876cc5a0fa0b1e036fa57e9765011c9262
European01 <emiryaziciofficial@gmail.com>
2024-02-05 21:03:35 -0500

    Add an example to practice.

--- src/lesson14_CustomClass/practices/Rectangle.java
+++ src/lesson14_CustomClass/practices/Rectangle.java
@@ -1,4 +1,29 @@
 package lesson14_CustomClass.practices;
 
 public class Rectangle {
+
+    public double length;
+    public double width;
+
+    public void setInfo(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+    public double calculateArea(){
+        return length*width;
+
+    }
+    public double calculatePerimeter(){
+        return  2*(length+width);
+
+    }
+
+    public String toString() {
+        return "Rectangle{" +
+                "length=" + length +
+                ", width=" + width +
+                ", area=" + calculateArea() +
+                ", perimeter=" + calculatePerimeter() +
+                '}';
+    }
 }
