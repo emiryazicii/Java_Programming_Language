cc1925b958e2072faf1de7be75ad6c685e4e7943
European01 <emiryaziciofficial@gmail.com>
2024-02-05 20:16:04 -0500

    Add an example to practice.

--- src/lesson13_NestedLoop/practices/Shape.java
+++ src/lesson13_NestedLoop/practices/Shape.java
@@ -1,4 +1,30 @@
 package lesson13_NestedLoop.practices;
 
 public class Shape {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
+/*
+1. Use a nested loop to print the following shape
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * * * *
+* * * * * * *
+* * * * * * * *
+* * * * * * * * *
+* * * * * * * * * *
+ */
+
