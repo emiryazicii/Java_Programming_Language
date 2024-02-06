8eaf980ed47fdec033306a64ef240e3ea3002506
European01 <emiryaziciofficial@gmail.com>
2024-02-05 20:48:24 -0500

    Add an example to practice.

--- src/lesson14_CustomClass/practices/Recipe.java
+++ src/lesson14_CustomClass/practices/Recipe.java
@@ -1,4 +1,25 @@
 package lesson14_CustomClass.practices;
 
 public class Recipe {
+
+    public String name;
+    public String ingredients;
+    public int servingSize;
+    public double cost;
+
+    public void setInfo(String name, String  ingredients, int servingSize, double cost) {
+        this.name = name;
+        this.ingredients = ingredients;
+        this.servingSize = servingSize;
+        this.cost = cost;
+    }
+
+    public double costPerPerson(){
+        return cost / servingSize ;
+    }
+
+    public String toString() {
+        return "Recipe for "+name+" will require these ingredients:\n"+ ingredients+"\nDish will serve "+servingSize+
+                " and cost a total of $"+cost+" to make.";
+    }
 }
