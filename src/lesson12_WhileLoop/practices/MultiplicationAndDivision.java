999b149f7afc6fe76a3b9d5ed7482e9ef29c477f
European01 <emiryaziciofficial@gmail.com>
2024-02-05 19:56:09 -0500

    Add an example to practice.

--- src/lesson12_WhileLoop/practices/MultiplicationAndDivision.java
+++ src/lesson12_WhileLoop/practices/MultiplicationAndDivision.java
@@ -1,4 +1,41 @@
 package lesson12_WhileLoop.practices;
 
 public class MultiplicationAndDivision {
+
+    public static void main(String[] args) {
+
+        System.out.println(multiplication(10,50));
+        System.out.println(division(2,5));
+        System.out.println(division(100,25));
+        System.out.println(division(12,0));
+    }
+    public static int multiplication(int num1, int num2) {
+
+        int result = 0;
+
+        for (int i = 1; i <= num2; i++) {
+
+            result += num1;
+        }
+        return result;
+    }
+    public static int division(int num1, int num2) {
+
+        int result = 0;
+
+        if (num2 == 0){
+
+            System.err.println("You can't divide any number by 0");
+        }
+        else if (num1 < num2){
+
+            return 0;
+        }
+        while (num1 >= num2) {
+
+            num1 -= num2;
+            result++;
+        }
+        return result;
+    }
 }
